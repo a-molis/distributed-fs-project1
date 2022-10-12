ff65e93d666e809c18bef92ab36232adafb1ced6
Yamack <49182980+Yamack@users.noreply.github.com>
2022-10-12 11:46:37 -0700

    start controller uses the correct controller hostname now (#46)
    Co-authored-by: Yamac Karakus <ykarakus@orion03.cs.usfca.edu>

--- scripts/start-controller.sh
+++ scripts/start-controller.sh
@@ -11,4 +11,4 @@ echo "file_system_dir :" ${file_system_dir}
 echo "controller_port :" ${storage_node_port}
 echo "storage_node_port :" ${storage_node_port}
 echo "Starting Controller..."
-ssh "${controller}" "cd ${file_system_dir} && pwd && ${file_system_dir}/dfs -type=controller -host=${HOSTNAME} -port=${controller_port} -id=${controller} &> "${log_dir}/controller.log" &"
+ssh "${controller}" "cd ${file_system_dir} && pwd && ${file_system_dir}/dfs -type=controller -host=${controller}.cs.usfca.edu -port=${controller_port} -id=${controller} &> "${log_dir}/controller.log" &"
